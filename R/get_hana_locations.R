c3157b7efaea1c23ac68b2417d4fd33b69877494
gottalottasoul <abbenante@yahoo.com>
2020-04-15 12:31:12 -0400

    updated with additional functions, added room sizes and additional locations

--- R/get_hana_locations.R
+++ R/get_hana_locations.R
@@ -3,10 +3,45 @@ function()
 {
  
 #create df with open dates
-locations<-c("Dallas, TX","Irvine, CA","St. Mary Axe","Hammersmith","St. Martin's Court")
-hana_name<-c("Hana at Park District","Hana at Park Place", "Hana at SMA", "Hana at Hammersmith", "Hana at SMC")
-hana_region<-c("US","US","UK","UK","UK")
-open_date<-c(as.Date("2019-07-29"),as.Date("2020-01-02"),as.Date("2020-02-17"),as.Date("2020-02-17"),as.Date("2019-12-06"))
+locations<-c("Dallas, TX",
+             "Irvine, CA",
+             "St. Mary Axe",
+             "Hammersmith","
+             St. Martin's Court",
+             "Philadelphia",
+             "Arlington",
+             "New York, NY",
+             "Windmill Green")
+hana_name<-c("Hana at Park District",
+             "Hana at Park Place", 
+             "Hana at SMA", 
+             "Hana at Hammersmith", 
+             "Hana at SMC",
+             "Hana at 1818 Market",
+             "Hana at Crystal City",
+             "Hana at WTC3",
+             "Hana at Windmill Green")
+hana_region<-c("US",
+               "US",
+               "UK",
+               "UK",
+               "UK",
+               "US",
+               "US",
+               "US",
+               "UK")
+open_date<-c(as.Date("2019-08-22"),
+             as.Date("2019-12-30"),
+             as.Date("2020-02-24"),
+             as.Date("2020-05-15"),
+             as.Date("2019-09-06"),
+             as.Date("2020-10-12"),
+             as.Date("2020-09-30"),
+             as.Date("2020-09-17"),
+             as.Date("2020-09-30")
+             )
+
+
 hana_locations<-data.frame(locations,hana_name,hana_region,open_date,stringsAsFactors = FALSE)
 
 hana_locations
---
+++ R/get_slack_payload.R
@@ -0,0 +1,47 @@
+get_slack_payload <- 
+function(report_name,report_url) {
+  
+  payload<-paste0('{
+  "channel": "#insights",
+  "blocks": [
+    {
+      "type": "section",
+      "block_id": "section567",
+      "text": {
+        "type": "mrkdwn",
+        "text": ":carlton: \n ',report_name,' has been updated.\n <',report_url,'>"
+      },
+      "accessory": {
+        "type": "image",
+        "image_url": "https://www.poimapper.com/wp-content/themes/POIMAPPER/assets/img/report_icon.png",
+        "alt_text": "Report image"
+      }
+    },
+    {
+      "type": "section",
+      "block_id": "section789",
+      "fields": [
+        {
+          "type": "mrkdwn",
+          "text": "*Last run*\n',
+                  lubridate::now(),
+                  '"
+        }
+      ]
+    },
+     {
+      "type": "section",
+      "block_id": "section999",
+      "fields": [
+        {
+          "type": "mrkdwn",
+          "text": "if you need help accessing the report, please reach out to <@UHVUF8FC4>"
+        }
+      ]
+    }
+  ]
+}')
+  
+  payload
+  
+}
