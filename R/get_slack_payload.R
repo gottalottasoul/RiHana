b4f2c56faf48ad255b788f865987a29ad8f20d82
gottalottasoul <abbenante@yahoo.com>
2020-06-01 15:51:19 -0400

    reformat slack message

--- R/get_slack_payload.R
+++ R/get_slack_payload.R
@@ -1,47 +1,40 @@
 get_slack_payload <- 
 function(report_name,report_url,report_updates='') {
     
-    payload<-paste0('{
-  "channel": "#insights",
-  "blocks": [
-    {
-      "type": "section",
-      "block_id": "section567",
-      "text": {
-        "type": "mrkdwn",
-        "text": ":carlton: \n ',report_name,' has been updated.\n <',report_url,'>. ',report_updates,'"
-      },
-      "accessory": {
-        "type": "image",
-        "image_url": "https://www.poimapper.com/wp-content/themes/POIMAPPER/assets/img/report_icon.png",
-        "alt_text": "Report image"
-      }
-    },
-    {
-      "type": "section",
-      "block_id": "section789",
-      "fields": [
-        {
-          "type": "mrkdwn",
-          "text": "*Last run*\n',
-                    lubridate::now(),
-                    '"
-        }
-      ]
-    },
-     {
-      "type": "section",
-      "block_id": "section999",
-      "fields": [
-        {
-          "type": "mrkdwn",
-          "text": "if you need help accessing the report, please reach out to <@UHVUF8FC4>"
-        }
-      ]
-    }
-  ]
+  payload<-paste0('{
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": ":carlton: *<',report_url,'|',report_name,'> has been updated*"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Last Run:',lubridate::now(),'"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*notes*: ',report_updates,'"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "_If you need help accessing this report, please reach out to <@UHVUF8FC4>_"
+			}
+		}
+	]
 }')
-    
+  
+  
   payload
     
 }
