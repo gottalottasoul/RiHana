e7bccf4194fe3a440267655ee184a818d169718f
gottalottasoul <abbenante@yahoo.com>
2020-06-01 16:20:18 -0400

    update with YTD value

--- R/get_relevant_date.R
+++ R/get_relevant_date.R
@@ -3,6 +3,7 @@ get_relevant_date <-function()
 
   relevenant_dates<-tibble::tibble(
     yesterdays_date = Sys.Date()-1,
+    ytd_date=lubridate::floor_date(Sys.Date(),unit="year"),
     yoy_date = lubridate::ceiling_date(Sys.Date()-365, unit="week",week_start = 1),
     past_30_days = Sys.Date()-31,
     past_7_days =  Sys.Date()-8,
